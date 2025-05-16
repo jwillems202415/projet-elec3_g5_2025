fe71133a3d67221620cbac895734000c2c9fac30
jwillems202415 <j.willems@students.ephec.be>
2025-05-16 14:09:04 +0200

    Updated the experimental module "display_number.py" The function contained within this file can now manage single digts such as 2, 8, 7... by converting them into 02, 08 and 07.
    Regarding numbers with more than two numbers, only the two last digits
    of the number will be displayed. For instance, "249" will show "49" on
    the displays.

--- experiments/display_number.py
+++ experiments/display_number.py
@@ -1,47 +1,50 @@
+# Call the following function to display a number on the two displays.
+
 from machine import Pin
 import time
 
-# Module taking as input a number then displays it continuously on the segmented displays.
-
 def display_number(display_value):
-
-    # Mapping of the four pins used to input a number into the decoder in BCD format, going from LSB to MSB
-    bcd_pins = [Pin(n, Pin.OUT) for n in [18,26,22,19]]
-
-    # Mapping of the two pins used to activate or not the two segment displays
+    bcd_pins = [Pin(n, Pin.OUT) for n in [18, 26, 22, 19]]
     digits = [Pin(27, Pin.OUT), Pin(28, Pin.OUT)]
 
-    # Table BCD 0–9, going from LSB → MSB
     BCD_CODES = [
-        [0,0,0,0],  # 0
-        [1,0,0,0],  # 1
-        [0,1,0,0],  # 2
-        [1,1,0,0],  # 3
-        [0,0,1,0],  # 4
-        [1,0,1,0],  # 5
-        [0,1,1,0],  # 6
-        [1,1,1,0],  # 7
-        [0,0,0,1],  # 8
-        [1,0,0,1],  # 9
+        [0, 0, 0, 0],  # 0
+        [1, 0, 0, 0],  # 1
+        [0, 1, 0, 0],  # 2
+        [1, 1, 0, 0],  # 3
+        [0, 0, 1, 0],  # 4
+        [1, 0, 1, 0],  # 5
+        [0, 1, 1, 0],  # 6
+        [1, 1, 1, 0],  # 7
+        [0, 0, 0, 1],  # 8
+        [1, 0, 0, 1],  # 9
     ]
 
-    # Receives a number then "encodes" it in a format understandable by the decoder
-    def set_bcd(valeur):
-        code = BCD_CODES[valeur]
+    def set_bcd(value):
+        code = BCD_CODES[value]
         for pin, bit in zip(bcd_pins, code):
             pin.value(bit)
-    
-    # Main loop
+
     while True:
-        
-        # Displays the left digit on the left display, then turns it off
-        set_bcd(int(str(display_value)[0]))
+        # Ensure the number is an integer
+        number = int(display_value)
+
+        # Extract digits
+        if number < 10:
+            left_digit = 0
+            right_digit = number
+        else:
+            left_digit = (number // 10) % 10
+            right_digit = number % 10
+
+        # Display left digit
+        set_bcd(left_digit)
         digits[0].value(1)
         time.sleep(0.005)
         digits[0].value(0)
-        
-        # Displays the right digit on the right display, then turns it off
-        set_bcd(int(str(display_value)[1]))
+
+        # Display right digit
+        set_bcd(right_digit)
         digits[1].value(1)
         time.sleep(0.005)
         digits[1].value(0)
